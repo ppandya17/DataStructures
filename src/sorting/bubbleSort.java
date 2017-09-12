1a400f1c86f8e750b2d1520d8161efa723c86084
Parth Pandya <pspandya2009@gmail.com>
2017-09-12 01:32:49 -0400

    Complete bubble Sort

--- src/sorting/bubbleSort.java
+++ src/sorting/bubbleSort.java
@@ -2,9 +2,31 @@ package sorting;
 
 public class bubbleSort {
 
+
+	static void bubbleSort(int[] array){
+		for(int i=0; i<array.length-1; i++){
+			for(int j=0; j< array.length-1-i; j++){
+				if(array[j] > array[j+1]){
+					int temp = array[j+1];
+					array[j+1] = array[j];
+					array[j] = temp;					
+				}			
+
+			}
+		}
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int array[] = new int[]{10,65,3,98,1,5,7,3,4,9,2,8,1,4,9,5,3,1,7,8,33};
+		bubbleSort(array);
+		
+		for(int i=0; i<array.length; i++){
+			if(i > 0){
+				System.out.print(", ");
+			}
+			System.out.print(array[i]);
+		}
 	}
 
 }
