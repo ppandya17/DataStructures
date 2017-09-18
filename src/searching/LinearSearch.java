090f46de4bb10eb13856320f95e7db1e90b06d0e
Parth Pandya <pspandya2009@gmail.com>
2017-09-18 18:56:25 -0400

    Linear Search

---
+++ src/searching/LinearSearch.java
@@ -0,0 +1,35 @@
+package searching;
+
+import java.util.stream.IntStream;
+
+public class LinearSearch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] array = new int[]{10,1,6,7,9,4,3,2,8,5};
+		int searchNumber = 2;
+		
+		linearSearch(array, searchNumber);
+	}
+	
+	static void linearSearch(int[] array, int number){
+		if(!IntStream.of(array).anyMatch(x -> x == number)){
+			printResult(false, number, (Integer) null);
+			System.exit(0);
+		}
+		for(int i=0; i < array.length-1; i++){
+			if(array[i] == number){
+				printResult(true, number, i);
+			}
+		}
+	}
+	
+	static void printResult(boolean result, int number, int position){
+		if (result == false){
+			System.out.println("Number id not in the array");
+		} else {
+			System.out.println("Number "+ number +" is at "+position+" position.");
+		}
+	}
+
+}
