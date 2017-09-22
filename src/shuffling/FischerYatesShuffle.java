d2066753436a6fbfd16a8bb37312e0a34395581f
Parth Pandya <pspandya2009@gmail.com>
2017-09-22 15:12:04 -0400

    Finished Fischer-Yates Shufflicng Algorithm

--- src/shuffling/FischerYatesShuffle.java
+++ src/shuffling/FischerYatesShuffle.java
@@ -1,10 +1,23 @@
 package shuffling;
 
+import java.util.Random;
+
 public class FischerYatesShuffle {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int[] array = new int[]{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+		
+		for(int i=array.length-1; i>=0; i--){
+			int pick = new Random().nextInt(i);
+			swap(array, i, pick);
+		}
+	}
+	
+	static void swap(int[] array, int i, int j){
+		int temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
 	}
 
 }
