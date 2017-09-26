53644406c02a199c73c97c2e840f4a8bad24e07a
Parth Pandya <pspandya2009@gmail.com>
2017-09-25 22:19:43 -0400

    updated using test cases

--- src/sorting/QuickSort.java
+++ src/sorting/QuickSort.java
@@ -1,20 +1,66 @@
 package sorting;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Random;
 
 public class QuickSort {
 
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		// TODO Auto-generated method stub
 		//int array[] = new int[]{10,5,3,4,9,2};
-		int array[] = new int[]{5, 4, 4, 4, 4, 1};
 		//int[] array = new int[10];
 		
 		//generateRandomArray(array);
 		
-		quickSort(array, 0, array.length-1);
+		readFromInputFile();
 		
-		printArray(array);		
+		
+		//quickSort(array, 0, array.length-1);
+		
+		//printArray(array);		
+	}
+	
+	static void readFromInputFile(){
+		try
+        {
+            File inputFile = new File("src/inputTests/hw2a.dat");
+            FileReader fr = new FileReader(inputFile);
+            BufferedReader br = new BufferedReader(fr);
+
+            int noOfTestCases = Integer.parseInt(br.readLine());
+            
+            for(int i = 0; i < noOfTestCases; i++)
+            {
+                int arraySize = Integer.parseInt(br.readLine());
+            
+                String a = br.readLine();
+                
+                String[] array = a.split(" ");
+                int[] myData = new int[arraySize];
+                for(int j = 0; j < arraySize; j++)
+                {
+                    myData[j] = Integer.parseInt(array[j]);
+                }
+                
+                System.out.println();
+            
+                quickSort(myData,0,arraySize - 1);
+                for(int j = 0; j < arraySize; j++)
+                {
+                    System.out.print(myData[j]+" ");
+                }
+                System.out.println();
+            }
+            br.close();
+        }
+        catch(IOException ex)
+        {
+            System.out.print(ex.toString());
+        }
+	
 	}
 
 	static void quickSort(int[] array, int left, int right){
