afefdb1bcc6c0a2360f02be060d9aec0d0866f84
Parth Pandya <pspandya2009@gmail.com>
2017-09-25 22:27:10 -0400

    updated using testfile

--- src/sorting/HeapSort.java
+++ src/sorting/HeapSort.java
@@ -1,19 +1,58 @@
 package sorting;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Random;
 
 public class HeapSort {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int array[] = new int[]{10,5,3,4,9,2};
+		//int array[] = new int[]{10,5,3,4,9,2};
 		//int array[] = new int[]{10,65,3,98,1,5,7,3,4,9,2,8,1,4,9,5,3,1,7,8,33};
 		//int[] array = new int[10];
 		
 		//generateRandomArray(array);
+		readFromInputFile();
 		
-		heapsort(array);
-		printArray(array);		
+		//heapsort(array);
+		//printArray(array);		
+	}
+	
+	static void readFromInputFile(){
+        try
+        {
+            File inputFile = new File("src/inputTests/hw2a.dat");
+            FileReader fr = new FileReader(inputFile);
+            BufferedReader br = new BufferedReader(fr);
+
+            int noOfTestCases = Integer.parseInt(br.readLine());
+            for(int i = 0; i < noOfTestCases; i++)
+            {
+                int arraySize = Integer.parseInt(br.readLine());
+                String a = br.readLine();
+                String[] array = a.split(" ");
+                int[] myData = new int[arraySize];
+                for(int j = 0; j < arraySize; j++)
+                {
+                    myData[j] = Integer.parseInt(array[j]);
+                }
+                System.out.println();
+                heapsort(myData);
+                for(int j = 0; j < arraySize; j++)
+                {
+                    System.out.print(myData[j]+" ");
+                }
+                System.out.println();
+            }
+            br.close();
+        }
+        catch(IOException ex)
+        {
+            System.out.print(ex.toString());
+        }
 	}
 
 	static void heapsort(int[] array){
